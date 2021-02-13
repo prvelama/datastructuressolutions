e8b4ceea594246dea0705a66cb8533b4fa5fcd7d
prvelama <34073640+prvelama@users.noreply.github.com>
2021-02-13 12:38:40 -0500

    Initial commit

---
+++ src/com/datastructures/solutions/BinarySearchTreeSolution.java
@@ -0,0 +1,8 @@
+package com.datastructures.solutions;
+
+public class BinarySearchTreeSolution {
+
+    public static void main(String[] args) {
+        System.out.println("Your first data structure solution in GIT");
+    }
+}
